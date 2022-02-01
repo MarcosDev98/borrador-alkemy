a994ecfa48e51fdd0446f60a188902c7f7652743
Marcos Aguado <marcos1903@live.com.ar>
2022-02-01 19:28:21 -0300

    revert

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
 
   return (
     <div>
-      Hola
       <Container>
         <Form />
       </Container>
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,16 +1,55 @@
 import React from 'react'
 import './Form.css'
-import { Input } from '../'
+import { Input } from '../../components'
+import useForm from '../../hooks/useForm'
 
-const Form = () => {
+const Form = ({ submit }) => {
+
+  const [form, handleChange, reset] = useForm({
+    concept: '',
+    amount: '',
+    date: '',
+    type: '',
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault(submit)
+    submit(form)
+    reset
+  }
+
   return (
-    <form>
-      <Input 
-        label='Concepto'
-        name='Concepto'
-        
-      />
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <Input 
+          label='Concepto'
+          name='Concept'
+          value={form.concept}
+          onChange={handleChange}
+        />
+
+        <Input
+          label='Monto'
+          name='amount'
+          value={form.amount}
+          onChange={handleChange}
+        />
+
+        <Input
+          label='Fecha'
+          name='date'
+          value={form.date}
+          onChange={handleChange}
+        />
+
+        <Input
+          label='Tipo'
+          name='type'
+          value={form.type}
+          onChange={handleChange}
+        />
+      </form>
+    </div>
   )
 }
 
