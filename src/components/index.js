12f401083531835c65fe7b3e796dd3cfaec0d3e8
Marcos Aguado <marcos1903@live.com.ar>
2022-02-02 11:37:24 -0300

    index file modified

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
-export { default as Form} from './Form'
 export { default as List } from './List'
 export { default as Container } from './Container'
+export { default as Input } from './Input'
+export { default as Card } from './Card'
+export { default as Button } from './Button'
