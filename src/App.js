06aeadefa4567aae2fcffdeb7601b9068bb8ba84
Marcos Aguado <marcos1903@live.com.ar>
2022-02-02 11:38:23 -0300

    project structure modified, App.js its the new main file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import React from 'react'
-import { Container, Form } from './components'
+import React, { useState } from 'react'
+import { Button, Card, Container, Input, List } from './components'
+import useForm from './hooks/useForm'
 
 
 function App() {
+
+  const [transactions, setTransaction] = useState([])
+  const [form, handleChange, reset] = useForm({
+    concept: '',
+    amount: '',
+    date: '',
+    type: '',
+  })
+
+  const submit = (e) => {
+    e.preventDefault()
+    setTransaction([
+      ...transactions,
+      form,
+    ])
+    reset()
+  }
+
+
   return (
-    <div>
+    <div style={{ marginTop: '5%' }}>
       <Container>
-        <Form />
+        <Card>
+          <div style={{ padding: 20 }}>
+            <form onSubmit={submit}>
+              <Input label='Concepto' name='concept' value={form.concept} onChange={handleChange} />
+              <Input label='Monto' name='amount' value={form.amount} onChange={handleChange} />
+              <Input label='Fecha' name='date' value={form.date} onChange={handleChange} />
+              <Input label='Tipo' name='type' value={form.type} onChange={handleChange} />
+        
+              <Button>Enviar</Button>
+            </form>
+          </div>
+        </Card>
+        <Card>
+          <List transactions={transactions}/>
+        </Card>
       </Container>
     </div>
   )
