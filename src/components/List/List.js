7a8584ea9efed9be9ce03419619aa02e5fb8fe78
Marcos Aguado <marcos1903@live.com.ar>
2022-02-16 10:39:27 -0300

    Balance calculado, operaciones separadas segun su tipo

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,50 @@
 import React from 'react';
+import { Card } from '../';
+import './List.css';
 
 const List = ({ transactions }) => {
 
+  // 1 y 2 esta hardcodeado.
+  // TODO: hacer metodo para buscar en la BD que id_type_transaction corresponde con INGRESO y cual con EGRESO.
+  const income = transactions.filter(x => x.id_type_transaction === 1);
+  const expenses = transactions.filter(x => x.id_type_transaction === 2);
+
+  const balance = () => {
+    const totalIncomes = income.map(x => x.amount);
+    const totalExpenses = expenses.map(x => x.amount);
+    let result = totalIncomes - totalExpenses;
+    return result;
+  };
+
   return (
-    <ul>
-      {transactions.map(x => 
-        <li key={x.id}>{`${x.concept} ${x.amount} ${x.date} ${x.user_id} ${x.id_type_transaction} ${x.is_deleted}`}</li>)}
-    </ul>
+    <div>
+      <div className='balance'>
+        <h2>Balance</h2>
+        <h3>{balance()}</h3>
+      </div>
+      <div className='list-container'>
+        <Card>
+          <div className='incomes'>
+            <h2>Ingresos</h2>
+            <ul>
+              {income.map(x => 
+                <li key={x.id}>{x.concept} {x.amount} {x.date} {x.user_id}</li>
+              )}
+            </ul>
+          </div>
+        </Card>
+        <Card>
+          <div className='expenses'> 
+            <h2>Egresos</h2>
+            <ul>
+              {expenses.map(x => 
+                <li key={x.id}>{x.concept} {x.amount} {x.date} {x.user_id}</li>
+              )}
+            </ul>
+          </div>
+        </Card>
+      </div>
+    </div>
   );
 };
 
