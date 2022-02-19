480ba8f041b3baeb086017e64a14cd6976add933
Marcos Aguado <marcos1903@live.com.ar>
2022-02-19 11:42:55 -0300

    TODO: arreglar rutas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, {  } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Container, Home } from './components';
+import { Container, Home, Login } from './components';
 
 
 function App() {
 
-  
+
+  const user = true;
+
   return (
     <div style={{ marginTop: '5%', marginBottom: '5%' }}>
       <Container>
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/' element={() => {
+            return user ? <Home /> : <Login />;
+          }} />
           
         </Routes>
       </Container>
