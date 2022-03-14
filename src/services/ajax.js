de69528840b5294c2d5149295e9fb4c2649dd990
Marcos Aguado <marcos1903@live.com.ar>
2022-03-14 19:45:26 -0300

    ajaxCreateUser() creado

--- src/services/ajax.js
+++ src/services/ajax.js
@@ -105,4 +105,28 @@ function ajaxLogin (credentials) {
 
 }
 
-export  { ajaxCreateTransaction, ajaxGetTransactions, ajaxGetTypes, ajaxLogin };
+function ajaxCreateUser (new_user) {
+  return new Promise(function(resolve, reject){
+    const http = new XMLHttpRequest();
+
+    http.onreadystatechange = () => {
+      if (http.readyState === 4 && http.status === 200) {
+      //
+        resolve(JSON.parse(http.responseText));
+      } 
+      else if (http.readyState === 4 && http.status === 401) {
+        reject(JSON.parse(http.responseText));
+      }
+    };
+    
+
+    http.open('POST', 'http://localhost:5005/api/login/', true);
+    http.setRequestHeader('Content-type', 'application/json');
+
+    http.send(JSON.stringify(new_user));
+
+  });
+}
+
+
+export  { ajaxCreateTransaction, ajaxGetTransactions, ajaxGetTypes, ajaxLogin, ajaxCreateUser };
