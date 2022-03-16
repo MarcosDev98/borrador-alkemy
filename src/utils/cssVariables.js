6f220342a74fea1de66fdb5311848a753bc0fe5a
Marcos Aguado <marcos1903@live.com.ar>
2022-03-16 11:10:21 -0300

    Se creo un archivo para guardar variables de colores y tamanios de fuentes para CSS.

---
+++ src/utils/cssVariables.js
@@ -0,0 +1,21 @@
+export const COLORS = {
+  button_bg: '#2185d0',
+  button_hover: '#2150d0',
+  container_bg: '#cecece',
+  card_bg: '#fff',
+  input_border: 'rgba(34, 34, 34, 0.15)',
+  input_outline: 'rgba(0, 132, 255 , 0.733)',
+  list_balance: '#64b6f5',
+  list_expenses: 'rgba(255,0,0, 0.7)',
+  list_incomes: 'rgba(0,255,0, 0.7);',
+  login_bg: '#4070f4',
+};
+
+
+export const FONTSIZES = {
+  button: '12px',
+  input: '14px',
+  list_items: '14px',
+  container: '14px',
+  login_title: '27px',
+};
