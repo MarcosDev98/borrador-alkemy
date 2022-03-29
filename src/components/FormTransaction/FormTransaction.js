d493a1f39019fd802bde1b49d216c7438361d3dc
Marcos Aguado <marcos1903@live.com.ar>
2022-03-29 12:04:41 -0300

    Se implemento el uso del componente Notification para mostrar errores.

--- src/components/FormTransaction/FormTransaction.js
+++ src/components/FormTransaction/FormTransaction.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { Card, Input, Select, Button } from '../'; 
-import { ajaxCreateTransaction, ajaxGetTransactions, ajaxGetTypes } from '../../services/ajax';
+import { Card, Input, Select, Button, Notification } from '../'; 
+import { ajaxCreateTransaction, ajaxGetTypes } from '../../services/ajax';
 
 import useForm from '../../hooks/useForm';
-const FormTransaction = () => {
 
-  const [transactions, setTransaction] = useState([]);
+
+const FormTransaction = ({ getTransactions }) => {
+
+  // const [transactions, setTransaction] = useState([]);
   const [user, setUser] = useState(null);
   const [types, setTypes] = useState([]);
+  const [notification, setNotification] = useState(false);
   const [form, handleChange, reset] = useForm({
     concept: '',
     amount: '',
@@ -15,45 +18,44 @@ const FormTransaction = () => {
     id_type_transaction: ''
   });
 
-  let loggedUser = '';
 
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    
     ajaxCreateTransaction(form, user.token)
-      .then((data) => {
-        setTransaction([...transactions, form]);
-        console.log('create_transaction_data', data);
+      .then(() => {
         getTransactions();
       })
-      .catch((error) => console.error('create_transaction_error', error));
+      .catch((error) => {
+        const newError = {
+          mode: 'error',
+          title: 'Ups',
+          message: 'Se produjo un error al guardar el registro.',
+        };
+        setNotification(newError);
+        console.error('create_transaction_error', error);
+      }
+      );
 
 
     reset();
   };
 
-  const getTransactions =  () => {
-    ajaxGetTransactions(loggedUser.token)
-      .then((data) => {
-
-        setTransaction(data);
-
-      })
-      .catch((error) => {
-        console.error('get_transactions_error', error);
-      });
-      
-  };
 
   const getTypes = () => {
     ajaxGetTypes()
       .then((data) => {
-
         setTypes(data);
       })
       .catch((error) => {
+        
+        const newError = {
+          mode: 'error',
+          title: 'Lo siento',
+          message: 'Hubo problemas durante la conexion con la BD.',
+        };
+        setNotification(newError);
         console.error('get_types_error', error);
       });
     
@@ -61,19 +63,22 @@ const FormTransaction = () => {
 
   useEffect(() => {
     getTypes();
-    loggedUser = window.sessionStorage.getItem('loggedUser');
-    setUser(loggedUser);
+    const loggedUser = JSON.parse(window.sessionStorage.getItem('loggedUser'));
+    if (loggedUser !== null) {
+      setUser(loggedUser);
+    }
   }, []);
+
 
   return (
     <Card>
+      {notification ? <Notification mode={notification.mode} title={notification.title} message={notification.message} /> : null}
       <div style={{ padding: 20 }}>
         <form onSubmit={handleSubmit}>
           <Input label='Concepto' name='concept' value={form.concept} onChange={handleChange} />
           <Input label='Monto' name='amount' value={form.amount} onChange={handleChange} />
           <Input label='Fecha' name='date' value={form.date} onChange={handleChange} />
           <Select label='Tipo' name='id_type_transaction' value={form.id_type_transaction} types={types} onChange={handleChange} />
-
           <Button>Enviar</Button>
         </form>
       </div>
